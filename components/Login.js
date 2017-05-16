879a86c9e3aac52d51044084026cb9d2cd2fab9f
Russell Osborne <russell@theironyard.com>
2017-05-16 10:56:09 -0400

    Added JWT Login

--- components/Login.js
+++ components/Login.js
@@ -7,13 +7,33 @@ class Login extends Component {
     this.state = { email: "", password: "" };
   }
 
-  focusNextField = (nextField) => {
+  focusNextField = nextField => {
     this.refs[nextField].focus();
   };
 
   getJWT = () => {
-    console.log('this.state', this.state);
-  }
+    console.log('JSON.stringify(...this.state)', JSON.stringify(this.state));
+
+    const request = new Request('http://localhost:3000/api/auth.json', {
+      method: "post",
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify(this.state)
+    });
+
+
+    fetch(request)
+      .then(function(response) {
+        // Convert to JSON
+        return response.json();
+      })
+      .then(function(j) {
+        // Yay, `j` is a JavaScript object
+        console.log(j);
+      });
+    console.log("this.state", this.state);
+  };
 
   render() {
     return (
@@ -27,15 +47,15 @@ class Login extends Component {
           placeholder="ie; john.doe@gmail.com"
           blurOnSubmit={false}
           onChangeText={email => this.setState({ email })}
-          onSubmitEditing={() => this.focusNextField('2')}
+          onSubmitEditing={() => this.focusNextField("2")}
         />
         <Text>Password</Text>
         <TextInput
-         ref='2'
+          ref="2"
           style={{ height: 40 }}
           placeholder="ie; red-pandas-123"
           secureTextEntry={true}
-          returnKeyType='go'
+          returnKeyType="go"
           onChangeText={password => this.setState({ password })}
           onSubmitEditing={this.getJWT}
         />
